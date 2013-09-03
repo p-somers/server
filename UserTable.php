5a6929498e22dae2ad4e48bdb6f2dde005db7d7b
p-somers <petersomers2@gmail.com>
2013-09-02 20:16:30 -0400

    minor fix

--- UserTable.php
+++ UserTable.php
@@ -8,19 +8,9 @@ class UserTable {
   private static $table = "users";
   public function allUsers() {
     try {
-      mysql_connect(self::$host,self::$user,self::$password);
-      mysql_select_db(self::$database);
-      $result = mysql_query("select * from ".self::$table);
-      if(!$result)
-        die('Invalid query: ' . mysql_error());
-      $arr = array();
-      foreach(mysql_fetch_object($result) as $key => $val) {
-        $arr[$key]=$val;
-      }
-      mysql_free_result($result);
-      mysql_close();
-      return $arr;
-    } catch( Exception $e ) {
+      $dbh = getDBH();
+      $dbh->prepare(
+    } catch( PDOException $e ) {
       return $e.getMessage();
     }
   }
