fb298b8d02fd4c026eb2100b921a3624eacf580c
p-somers <petersomers2@gmail.com>
2014-04-04 16:27:32 -0400

    Update index.php
    fixed tabs

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
-	<head>
+  <head>
     <!--link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css"-->
     <link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative:400,700,900' rel='stylesheet' type='text/css'>
-	  <link rel="stylesheet" type="text/css" href="./css/main.css">
-	  <link rel="stylesheet" type="text/css" href="./css/dialog.css">
-		<title>
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+    <link rel="stylesheet" type="text/css" href="./css/dialog.css">
+    <title>
       <?php
         $words = array('awesome','superb','sublime','elegant','splendid');
         $index = rand(0,count($words)-1);
@@ -24,37 +24,37 @@
         }
       });
     </script>
-	</head>
-	<body>
-	  <div id="wrapper">
-	      <span id="user"><a href="javascript:showLogin();">login</a></span>
-	      <div id="login_dialog">
-	        <label>Username:</label><input type="text" id="username"></input></br>
-	        <label>Password:</label><input type="password" id="password"></input>
-	        <button type="button" onClick="javascript:login();">ok</button>
-	      </div>
-	    <div id="header">
-	      <span>cool stuff</span>
-	    </div>
-      <div id="content_wrapper">
-        <div id="menu">
-          <div class="menu_item" id="main">Main</div>
-          <div class="menu_item" id="about">About</div>
-          <div class="menu_item" id="stuff">...stuff.</div>
-          <div class="menu_item" id="contact">Contact Me</div>
-        </div>
-        <div id="content">
-          <?php
-            include("php/getContents.php");
-            if(isset($_GET['target'])) {
-              echoFile(filter_input(INPUT_GET,'target'));
-            }
-            else
-              echoFile('main.html');
-          ?>
-        </div>
-		  </div>
-		  <div id="footer">Random coding stuff by Peter Somers</div>
-		</div>
-	</body>
+  </head>
+  <body>
+    <div id="wrapper">
+      <span id="user"><a href="javascript:showLogin();">login</a></span>
+      <div id="login_dialog">
+        <label>Username:</label><input type="text" id="username"></input></br>
+        <label>Password:</label><input type="password" id="password"></input>
+        <button type="button" onClick="javascript:login();">ok</button>
+      </div>
+    <div id="header">
+      <span>cool stuff</span>
+    </div>
+    <div id="content_wrapper">
+      <div id="menu">
+        <div class="menu_item" id="main">Main</div>
+        <div class="menu_item" id="about">About</div>
+        <div class="menu_item" id="stuff">...stuff.</div>
+        <div class="menu_item" id="contact">Contact Me</div>
+      </div>
+      <div id="content">
+        <?php
+          include("php/getContents.php");
+          if(isset($_GET['target'])) {
+            echoFile(filter_input(INPUT_GET,'target'));
+          }
+          else
+            echoFile('main.html');
+        ?>
+      </div>
+    </div>
+    <div id="footer">Random coding stuff by Peter Somers</div>
+    </div>
+  </body>
 </html>
